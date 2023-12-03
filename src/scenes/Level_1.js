8d97b13f1198035cd40b84c54dfce2ea0e27267e
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2023-12-02 21:47:13 -0800

    Adding more buildings to level 1

--- src/scenes/Level_1.js
+++ src/scenes/Level_1.js
@@ -5,14 +5,14 @@ class Level_1 extends Phaser.Scene {
 
     create() {
         console.log("LEVEL1")
-        this.background = this.add.tileSprite(0,0,640,480, "background").setOrigin(0,0);
+        this.background = this.add.tileSprite(0,0,1250,480, "background").setOrigin(0,0);
         this.cook = new Cook(this, 40, 80, 'cook', 0, 1);
         this.cook.body.setSize(20, 30);
         this.keys = this.input.keyboard.createCursorKeys();
 
-        this.cameras.main.setBounds(0, 0, 540, this.background.heightInPixels)
+        this.cameras.main.setBounds(0, 0, 1000, this.background.heightInPixels)
         this.cameras.main.startFollow(this.cook, true, 0.25, 0)
-        this.physics.world.setBounds(0, 0, 540, this.background.heightInPixels)
+        this.physics.world.setBounds(0, 0, 1000, this.background.heightInPixels)
 
         //set builidng1 
         this.building1 = this.physics.add.sprite(40, game_height - 50, "brownBuilding");
@@ -35,8 +35,24 @@ class Level_1 extends Phaser.Scene {
         this.building5 = this.physics.add.sprite(500, game_height - 50, "brownBuilding")
         this.building5.body.setImmovable(true);
 
+        //set building6
+        this.building6 = this.physics.add.sprite(650, game_height - 50, "blueBuilding")
+        this.building6.setScale(1.5);
+        this.building6.body.setImmovable(true);
+
+        //set building6
+        this.building7 = this.physics.add.sprite(800, game_height - 50, "blueBuilding")
+        this.building7.setScale(1.5);
+        this.building7.body.setImmovable(true);
+        this.building7.setTint(0xffff00)
+
+        //set building7
+        this.building8 = this.physics.add.sprite(950, game_height - 60, "blueBuilding")
+        this.building8.setScale(1.5);
+        this.building8.body.setImmovable(true);
+
         buildings = this.add.group([this.building1, this.building2, this.building3,
-        this.building4, this.building5]);
+        this.building4, this.building5, this.building6, this.building7, this.building8]);
 
         //add colliders
         this.physics.add.collider(this.cook, buildings, (cook, buildings) => {
